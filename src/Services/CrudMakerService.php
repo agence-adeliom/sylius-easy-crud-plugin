13d4b08582eae93ebbb5fd72ed91628bb27e11c5
Jérôme Engeln <jerome.engeln@adeliom.com>
2024-07-22 14:05:27 +0200

    feat: add mode parameters to crudmakerservice functions
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Services/CrudMakerService.php
+++ src/Services/CrudMakerService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Adeliom\SyliusEasyCrudPlugin\Services;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Sylius\Component\Resource\Model\ResourceInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Bundle\MakerBundle\Generator;
@@ -43,60 +44,78 @@ class CrudMakerService
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    public function generateAdmin(): ClassNameDetails
-    {
-        return $this->generateFileFromTpm('Admin');
+    public function generateAdmin(
+        ?string $suffix = null,
+        ?string $className = null,
+        ?string $templatePath = null,
+        ?array $variables = [],
+    ): ClassNameDetails {
+        return $this->generateFileFromTpm('Admin', $suffix, $className, $templatePath, $variables);
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    public function generateController(): ClassNameDetails
-    {
-        return $this->generateFileFromTpm('Controller');
+    public function generateController(
+        ?string $suffix = null,
+        ?string $className = null,
+        ?string $templatePath = null,
+        ?array $variables = [],
+    ): ClassNameDetails {
+        return $this->generateFileFromTpm('Controller', $suffix, $className, $templatePath, $variables);
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    public function generateEntity(string $className): void
+    public function generateEntity(string $className, ?string $template = null, ?array $variables = []): void
     {
         $this->generator->generateClass(
             $className,
-            __DIR__ . '/../Resources/skeleton/Entity.tpl.php',
-            [],
+            (is_string($template) && file_exists($template)) ? $template : __DIR__ . '/../Resources/skeleton/Entity.tpl.php',
+            $variables,
         );
         $this->generator->writeChanges();
         $this->namespaces['entity'] = $className;
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    public function generateEntityTranslation(string $className): void
+    public function generateEntityTranslation(string $className, ?string $template = null, ?array $variables = []): void
     {
         $this->generator->generateClass(
             $className . 'Translation',
-            __DIR__ . '/../Resources/skeleton/Translation.tpl.php',
-            [],
+            (is_string($template) && file_exists($template)) ? $template : __DIR__ . '/../Resources/skeleton/Translation.tpl.php',
+            $variables,
         );
         $this->generator->writeChanges();
         $this->namespaces['entityTranslation'] = $className . 'Translation';
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    public function generateRepository(string $className): void
+    public function generateRepository(string $className, ?string $template = null, ?array $variables = []): void
     {
         $this->generator->generateClass(
             str_replace('Entity', 'Repository', $className) . 'Repository',
-            __DIR__ . '/../Resources/skeleton/Repository.tpl.php',
-            [
-                'entity_name' => Str::getShortClassName($className),
-            ],
+            (is_string($template) && file_exists($template)) ? $template : __DIR__ . '/../Resources/skeleton/Repository.tpl.php',
+            array_merge([
+                            'entity_name' => Str::getShortClassName($className),
+                        ], $variables),
         );
         $this->generator->writeChanges();
     }
@@ -137,28 +156,35 @@ class CrudMakerService
     }
 
     /**
+     * @param array<string, mixed> $variables
+     *
      * @throws \Exception
      */
-    protected function generateFileFromTpm(string $template, ?string $suffix = null, ?string $className = null): ClassNameDetails
-    {
+    protected function generateFileFromTpm(
+        string $templateName,
+        ?string $suffix = null,
+        ?string $className = null,
+        ?string $templatePath = null,
+        ?array $variables = [],
+    ): ClassNameDetails {
         if (null === $suffix) {
-            $suffix = $template;
+            $suffix = $templateName;
         }
         $file = $this->generator->createClassNameDetails(
             $className ?? $this->entity->getShortName(),
-            $template,
+            $templateName,
             $suffix,
         );
         $this->generator->generateClass(
             $file->getFullName(),
-            __DIR__ . '/../Resources/skeleton/' . $template . '.tpl.php',
-            [
-                'entity' => $this->entity,
-                'repository' => $this->repository,
-            ],
+            (is_string($templatePath) && file_exists($templatePath)) ? $templatePath : __DIR__ . '/../Resources/skeleton/' . $templateName . '.tpl.php',
+            array_merge([
+                            'entity' => $this->entity,
+                            'repository' => $this->repository,
+                        ], $variables),
         );
         $this->generator->writeChanges();
-        $this->namespaces[strtolower($template)] = $file->getFullName();
+        $this->namespaces[strtolower($templateName)] = $file->getFullName();
 
         return $file;
     }
@@ -207,8 +233,14 @@ class CrudMakerService
     /**
      * @param array<string, mixed> $data
      */
-    private function appendRoutingConfig(array &$data, string $entityName): void
+    private function appendRoutingConfig(array &$data, string $entityName, ?string $translationPrefix = null, ?string $icon = null): void
     {
+        if (null === $translationPrefix) {
+            $translationPrefix = 'sylius_easy_crud_plugin';
+        }
+        if (null === $icon) {
+            $icon = 'file';
+        }
         $resourceDataBlock =
             'alias: ' . mb_strtolower($this->namespace . '.' . Str::asSnakeCase($entityName)) . "\n"
             . "section: admin\n"
@@ -221,17 +253,17 @@ class CrudMakerService
             . "        context: \$context\n"
             . "vars:\n"
             . "    all:\n"
-            . "        icon: 'file'\n"
-            . "        subheader: sylius_easy_crud_plugin.admin.ui.default.subheader\n"
-            . "        breadcrumb: sylius_easy_crud_plugin.admin.ui.default.index\n"
+            . "        icon: '" . $icon . "'\n"
+            . '        subheader: ' . $translationPrefix . ".admin.ui.default.subheader\n"
+            . '        breadcrumb: ' . $translationPrefix . ".admin.ui.default.index\n"
             . "        templates:\n"
             . "            form: \"@SyliusEasyCrudPlugin\\\\crud\\\\form\\\\_form.html.twig\"\n"
             . "    index:\n"
-            . "        header: sylius_easy_crud_plugin.admin.ui.default.index\n"
+            . '        header: ' . $translationPrefix . ".admin.ui.default.index\n"
             . "    create:\n"
-            . "        header: sylius_easy_crud_plugin.admin.ui.default.create\n"
+            . '        header: ' . $translationPrefix . ".admin.ui.default.create\n"
             . "    update:\n"
-            . "        header: sylius_easy_crud_plugin.admin.ui.default.update\n"
+            . '        header: ' . $translationPrefix . ".admin.ui.default.update\n"
             . "        redirect:\n"
             . "            route: update\n"
             . "            parameters:\n"
@@ -290,5 +322,33 @@ class CrudMakerService
                 ],
             ];
         }
+    }
+
+    /**
+     * @return array<int, mixed>
+     */
+    public static function getEntity(string $class, Generator $generator, ManagerRegistry $managerRegistry): array
+    {
+        $entity = null;
+        $entityTranslation = null;
+        $repository = null;
+        /**
+         * @var class-string<object> $classTranslation
+         */
+        $classTranslation = $class . 'Translation';
+        if (\class_exists($class)) {
+            $entity = new \ReflectionClass($class);
+
+            $repository = new \ReflectionClass($managerRegistry->getRepository($entity->getName()));
+            if (0 !== \mb_strpos($repository->getName(), $generator->getRootNamespace())) {
+                // not using a custom repository
+            }
+
+            if (\class_exists($classTranslation)) {
+                $entityTranslation = new \ReflectionClass($classTranslation);
+            }
+        }
+
+        return [$entity, $entityTranslation, $repository];
     }
 }
