1093b1fc0890ef682bd8e96eaa50fbfeb1a350a9
Jérôme Engeln <jerome.engeln@adeliom.com>
2024-07-24 16:13:28 +0200

    30 feature request move autocomplete method to a trait (#31)
    * feat: add resource autocomplete trait
    
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>
    
    * feat: use trait
    
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>
    
    * fix :errors
    
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>
    
    ---------
    
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Controller/SyliusCrudResourceController.php
+++ src/Controller/SyliusCrudResourceController.php
@@ -7,14 +7,10 @@ namespace Adeliom\SyliusEasyCrudPlugin\Controller;
 use Adeliom\SyliusEasyCrudPlugin\Admin\AbstractAdmin;
 use Adeliom\SyliusEasyCrudPlugin\CrudFactory\Config\Crud;
 use FOS\RestBundle\View\View;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Sylius\Bundle\ResourceBundle\Grid\View\ResourceGridView;
-use Sylius\Component\Resource\Metadata\Metadata;
 use Sylius\Component\Resource\ResourceActions;
-use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -22,6 +18,8 @@ use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class SyliusCrudResourceController extends ResourceController
 {
+    use ResourceAutocompleteTrait;
+
     public function indexAction(Request $request): Response
     {
         /**
@@ -122,65 +120,6 @@ class SyliusCrudResourceController extends ResourceController
         }
 
         return $this->createRestView($configuration, $resource);
-    }
-
-    /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
-    public function autocompleteAction(Request $request): Response
-    {
-        // set metadata for resource that need an autocomplete search
-        $resourceName = $request->query->get('resourceName');
-        $repositoryMethod = $request->query->get('repositoryMethod');
-        $repositoryArguments = [];
-        if (is_string($request->query->get('repositoryArguments'))) {
-            $repositoryArguments = json_decode(
-                $request->query->get('repositoryArguments'),
-                true,
-            );
-        }
-
-        $serializationGroups = [];
-        if (is_string($request->query->get('serializationGroups'))) {
-            $serializationGroups = json_decode(
-                $request->query->get('serializationGroups')
-            );
-        }
-
-        try {
-            /** @var array<mixed> $resources */
-            $resources = $this->getParameter('sylius.resources');
-        } catch (InvalidArgumentException $exception) {
-            throw new HttpException(Response::HTTP_FORBIDDEN, $exception->getMessage());
-        }
-
-        foreach ($resources as $alias => $configuration) {
-            if ($resourceName === $alias) {
-                $this->metadata = Metadata::fromAliasAndConfiguration($alias, $configuration);
-                $syliusRepositoryService = str_replace('sylius.', 'sylius.repository.', $alias);
-                if ($this->container->has($syliusRepositoryService)) {
-                    $this->repository = $this->container->get($syliusRepositoryService);
-                } else {
-                    $this->repository = $this->container->get('doctrine')
-                        ->getRepository($configuration['classes']['model']);
-                }
-            }
-        }
-
-        $request->attributes->set('_format', 'json');
-        $request->attributes->set('_sylius', [
-            'serialization_groups' => $serializationGroups ?? [
-                'Autocomplete',
-            ],
-            'permission' => true,
-            'repository' => [
-                'method' => $repositoryMethod,
-                'arguments' => $repositoryArguments,
-            ],
-        ]);
-
-        return $this->indexAction($request);
     }
 
     public function exportGridResourcesTestGroupAction(
