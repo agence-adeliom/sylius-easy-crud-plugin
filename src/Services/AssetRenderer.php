fe93efeb28f774906c5976b292e53f37196e84a7
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-01-31 10:30:02 +0100

    feat: create asset renderer service
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

---
+++ src/Services/AssetRenderer.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Adeliom\SyliusEasyCrudPlugin\Services;
+
+use Adeliom\SyliusEasyCrudPlugin\CrudFactory\Config\Asset;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Event\RenderAssetTagEvent;
+
+class AssetRenderer
+{
+    public function __construct(
+        private EventDispatcherInterface $eventDispatcher,
+        private TagRenderer $tagRenderer,
+    )
+    {}
+
+    /**
+     * @param array{js: array<string|Asset>|null, css: array<string|Asset>|null, webpack: array<string|Asset>|null} $assets
+     */
+    public function renderAssets(array $assets): string
+    {
+        $html = '';
+
+        if (!empty($this->assets['css'])) {
+
+            foreach ($this->assets['css'] as $asset) {
+                $attributes = [];
+                $attributes['rel'] = 'stylesheet';
+                if (is_string($asset)) {
+                    $attributes['href'] = $asset;
+                } elseif ($asset instanceof Asset) {
+                    $attributes['href'] = $asset->getAsDto()->getValue();
+                }
+
+                $event = new RenderAssetTagEvent(
+                    RenderAssetTagEvent::TYPE_SCRIPT,
+                    $attributes['src'],
+                    $attributes
+                );
+                if (null !== $this->eventDispatcher) {
+                    $event = $this->eventDispatcher->dispatch($event);
+                }
+                $attributes = $event->getAttributes();
+
+                $html .= sprintf(
+                    '<link %s>',
+                    $this->convertArrayToAttributes($attributes)
+                );
+            }
+        }
+
+        if (!empty($this->assets['js'])) {
+            foreach ($this->assets['js'] as $asset) {
+                $attributes = [];
+                if (is_string($asset)) {
+                    $attributes['src'] = $asset;
+                } elseif ($asset instanceof Asset) {
+                    $attributes['src'] = $asset->getAsDto()->getValue();
+                }
+
+                $event = new RenderAssetTagEvent(
+                    RenderAssetTagEvent::TYPE_SCRIPT,
+                    $attributes['src'],
+                    $attributes
+                );
+                if (null !== $this->eventDispatcher) {
+                    $event = $this->eventDispatcher->dispatch($event);
+                }
+                $attributes = $event->getAttributes();
+
+                $html .= sprintf(
+                    '<script %s></script>',
+                    $this->convertArrayToAttributes($attributes)
+                );
+            }
+        }
+
+        if (!empty($this->assets['webpack'])) {
+            foreach ($this->assets['webpack'] as $webpackAsset) {
+                try {
+                    if ($webpackAsset instanceof Asset) {
+                        $html .= $this->tagRenderer
+                            ->renderWebpackScriptTags(
+                                $webpackAsset->getAsDto()->getValue(),
+                                $webpackAsset->getAsDto()->getPackageName(),
+                                $webpackAsset->getAsDto()->getWebpackEntrypointName(),
+                                []
+                            );
+                        $html .= $this->tagRenderer
+                            ->renderWebpackLinkTags(
+                                $webpackAsset->getAsDto()->getValue(),
+                                $webpackAsset->getAsDto()->getPackageName(),
+                                $webpackAsset->getAsDto()->getWebpackEntrypointName(),
+                                []
+                            );
+                        continue;
+                    } else if (is_string($webpackAsset)) {
+                        $html .= $this->tagRenderer
+                            ->renderWebpackScriptTags(
+                                $webpackAsset,
+                                null,
+                                null,
+                                []
+                            );
+                        $html .= $this->tagRenderer
+                            ->renderWebpackLinkTags(
+                                $webpackAsset,
+                                null,
+                                null,
+                                []
+                            );
+                    }
+                } catch (\Exception) {
+                    $html .= '';
+                }
+            }
+        }
+
+        return $html;
+    }
+
+
+    private function convertArrayToAttributes(array $attributesMap): string
+    {
+        // remove attributes set specifically to false
+        $attributesMap = array_filter($attributesMap, static function ($value) {
+            return false !== $value;
+        });
+
+        return implode(' ', array_map(
+            static function ($key, $value) {
+                // allows for things like defer: true to only render "defer"
+                if (true === $value || null === $value) {
+                    return $key;
+                }
+
+                return sprintf('%s="%s"', $key, htmlentities($value));
+            },
+            array_keys($attributesMap),
+            $attributesMap
+        ));
+    }
+
+}
