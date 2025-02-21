6c42cd7375ed0652432ddbfeefd591459001d320
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-02-21 18:02:25 +0100

    wip: create + update layout with form and menu
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/CrudFactory/CrudAdminFactory.php
+++ src/CrudFactory/CrudAdminFactory.php
@@ -207,8 +207,7 @@ class CrudAdminFactory
         $vars = [];
         $vars['form_themes'] = array_values(
             [
-                '@SyliusAdmin/shared/form_theme.html.twig',
-                '@SyliusEasyCrudPlugin/shared/crud/common/content/form/form_theme.html.twig',
+                '@SyliusAdmin/shared/form_theme.html.twig'
             ]
             + $this->formThemes,
         );
