9c417bbff48406b805ef5946c129bcad249b9a53
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2025-02-21 11:46:54 +0100

    put back the create button on index page

--- src/Admin/AbstractAdmin.php
+++ src/Admin/AbstractAdmin.php
@@ -47,10 +47,12 @@ abstract class AbstractAdmin extends AbstractFormType implements AdminInterface
         $actions->setMetadata($this->crudAdminFactory->getMetadata());
         $actions
             ->addBatchAction(Action::BATCH_DELETE)
+
             ->addItemAction(Crud::PAGE_INDEX, Action::EDIT)
             ->addItemAction(Crud::PAGE_INDEX, Action::DELETE)
             ->addItemAction(Crud::PAGE_INDEX, Action::DETAIL)
 
+            ->addGlobalAction(Crud::PAGE_INDEX, Action::NEW)
             ->addGlobalAction(Crud::PAGE_DETAIL, Action::EDIT)
             ->addGlobalAction(Crud::PAGE_DETAIL, Action::INDEX)
         ;
