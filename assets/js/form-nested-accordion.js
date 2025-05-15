8c915578adc260e517d0d46e6d06701cf58f6854
Jérôme Engeln <jerome.engeln@adeliom.com>
2025-05-15 18:07:24 +0200

    feat: last file version 1.x
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- assets/js/form-nested-accordion.js
+++ assets/js/form-nested-accordion.js
@@ -1,53 +1,52 @@
-
-
 window.addEventListener('DOMContentLoaded', () => {
-  const initAccordion = function(target) {
-    if (target
-      .find('.ui.accordion').length > 0) {
-      const id = target
-        .find('.ui.accordion').data('accordion-id');
-      const accordion = target
-        .find('.ui.accordion');
-      accordion.accordion('destroy');
-      accordion
-        .accordion({
-          selector: {
-            accordion: '.accordion',
-            title: '.title',
-            trigger: '.title',
-            content: '.content',
-          },
-          animateChildren: false,
-          duration: 0,
-          namespace: id,
-          name: id,
-        });
+    const initAccordion = function(target) {
+        if (target
+            .find('.ui.accordion').length > 0) {
+            const id = target
+                .find('.ui.accordion').data('accordion-id');
+            const accordion = target
+                .find('.ui.accordion');
+            accordion.accordion('destroy');
+            accordion
+                .accordion({
+                    selector: {
+                        accordion: '.accordion',
+                        title: '.title',
+                        trigger: '.title',
+                        content: '.content',
+                    },
+                    animateChildren: false,
+                    duration: 0,
+                    namespace: id,
+                    name: id,
+                });
+        }
+    };
+
+    const collectionFirstItems = document.querySelectorAll("[data-form-collection=\"item\"]:first-child");
+    if (collectionFirstItems.length > 0) {
+        collectionFirstItems.forEach(function (item) {
+            const observer = new MutationObserver(function(event) {
+                setTimeout(function() {
+                    initAccordion($(event.target));
+                },1);
+            });
+
+            observer.observe(item.closest('.collection-list'), {
+                subtree: true,
+                childList: true,
+            });
+
+        })
     }
-  };
 
-  const collectionFirstItems = document.querySelectorAll("[data-form-collection=\"item\"]:first-child");
-  if (collectionFirstItems.length > 0) {
-    collectionFirstItems.forEach(function (item) {
-      const observer = new MutationObserver(function(event) {
-        setTimeout(function() {
-          initAccordion($(event.target));
-        },1);
-      });
-
-      observer.observe(item.closest('.collection-list'), {
-        subtree: true,
-        childList: true,
-      });
-
-    })
-  }
-
-  // init after default sylius script
-  // look at $('.ui.accordion').accordion(); in admin-entry.js
-  setTimeout(() => {
-    document.querySelectorAll('[data-form-collection="item"]')
-      .forEach(function (selector) {
-        initAccordion($(selector));
-      });
-  },1);
+    // init after default sylius script
+    // look at $('.ui.accordion').accordion(); in admin-entry.js
+    setTimeout(() => {
+        document.querySelectorAll('[data-form-collection="item"]')
+            .forEach(function (selector) {
+                $('.ui.accordion', $('.w-flexible-content').parents('form')).accordion('destroy');
+                initAccordion($(selector));
+            });
+    },1);
 });
