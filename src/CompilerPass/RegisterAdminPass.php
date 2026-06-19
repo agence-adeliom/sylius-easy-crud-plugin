b1feebc162a2f32730ea4500023b08368f99eca5
Jean-David Daviet <jeandaviddaviet@gmail.com>
2026-06-19 16:10:08 +0200

    Use AsAdmin resource class for inherited admins

--- src/CompilerPass/RegisterAdminPass.php
+++ src/CompilerPass/RegisterAdminPass.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Adeliom\SyliusEasyCrudPlugin\CompilerPass;
 
+use Adeliom\SyliusEasyCrudPlugin\Metadata\AsAdmin;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -18,10 +19,45 @@ class RegisterAdminPass implements CompilerPassInterface
             /** @var class-string|null $class */
             $class = $definition->getClass();
             if ($class) {
-                $definition->addArgument($class::getEntityFqcn());
+                $definition->addArgument($this->resolveResourceClass($class));
                 $definition->addArgument([]);
                 $definition->setAutowired(true);
             }
         }
+    }
+
+    /**
+     * @param class-string $class
+     */
+    private function resolveResourceClass(string $class): string
+    {
+        $reflectionClass = new \ReflectionClass($class);
+        if (
+            !$this->declaresEntityFqcn($reflectionClass) &&
+            [] !== $attributes = $reflectionClass->getAttributes(AsAdmin::class)
+        ) {
+            $attribute = $attributes[0]->newInstance();
+            if ($attribute instanceof AsAdmin && null !== $attribute->resourceClass) {
+                return $attribute->resourceClass;
+            }
+        }
+
+        if (!method_exists($class, 'getEntityFqcn')) {
+            throw new \LogicException(sprintf('The easy-crud admin "%s" must define getEntityFqcn() or #[AsAdmin(resourceClass: ...)].', $class));
+        }
+
+        /** @var string $resourceClass */
+        $resourceClass = $class::getEntityFqcn();
+
+        return $resourceClass;
+    }
+
+    /**
+     * @param \ReflectionClass<object> $reflectionClass
+     */
+    private function declaresEntityFqcn(\ReflectionClass $reflectionClass): bool
+    {
+        return $reflectionClass->hasMethod('getEntityFqcn') &&
+            $reflectionClass->getMethod('getEntityFqcn')->getDeclaringClass()->getName() === $reflectionClass->getName();
     }
 }
