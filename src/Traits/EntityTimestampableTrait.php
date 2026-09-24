77b0110305c2df247f1cd7669cca3e03a8ad4234
Jérôme Engeln <jerome.engeln@adeliom.com>
2026-09-24 09:55:18 +0200

    fix: set createdAt as immutable by default
    Signed-off-by: Jérôme Engeln <jerome.engeln@adeliom.com>

--- src/Traits/EntityTimestampableTrait.php
+++ src/Traits/EntityTimestampableTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Adeliom\SyliusEasyCrudPlugin\Traits;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -11,12 +12,12 @@ use Symfony\Component\Serializer\Annotation\Groups;
 trait EntityTimestampableTrait
 {
     #[Groups('main')]
-    #[ORM\Column(type: \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
     #[Gedmo\Timestampable(on: 'create')]
     protected \DateTimeInterface $createdAt;
 
     #[Groups('main')]
-    #[ORM\Column(type: \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE)]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     #[Gedmo\Timestampable(on: 'update')]
     protected \DateTimeInterface $updatedAt;
 
